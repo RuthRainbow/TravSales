423be141ab46b24d540a9ca63b76715b43c9a33c
RuthRainbow <ruth.king1991@gmail.com>
2013-03-17 14:12:07 +0000

    Put general algorithm in a separate package

--- src/main/java/org/bradheintz/travsales/ChromosomeScorer.java
+++
@@ -1,8 +0,0 @@
-package org.bradheintz.travsales;
-
-public abstract class ChromosomeScorer {
-
-		public ChromosomeScorer(String problem) {}
-
-	    protected abstract double score(String chromosomeString) throws InterruptedException;
-}
